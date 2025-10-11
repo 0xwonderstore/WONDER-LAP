fae31944d8b18a1fc80e1d353d2d7025ad0d784d
amrisa202 <amrisa202@gmail.com>
2025-10-11 18:04:23 +0000

    Changes before Firebase Studio auto-run

--- src/stores/languageStore.ts
+++ src/stores/languageStore.ts
@@ -1,4 +1,5 @@
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 import { translations } from '../translations';
 
 type Language = 'en' | 'ar';
@@ -9,8 +10,23 @@ interface LanguageState {
   setLanguage: (language: Language) => void;
 }
 
-export const useLanguageStore = create<LanguageState>((set) => ({
-  language: 'ar',
-  translations: translations.ar,
-  setLanguage: (language) => set({ language, translations: translations[language] }),
-}));
+export const useLanguageStore = create<LanguageState>()(
+  persist(
+    (set) => ({
+      language: 'ar',
+      translations: translations.ar,
+      setLanguage: (language) => set({ language, translations: translations[language] }),
+    }),
+    {
+      name: 'language-storage', // name of the item in the storage (must be unique)
+      getStorage: () => localStorage, // (optional) by default, 'localStorage' is used
+      onRehydrateStorage: (state) => {
+        // This function is called when the state is rehydrated from storage
+        // We need to manually set the translations based on the rehydrated language
+        if (state) {
+          state.translations = translations[state.language];
+        }
+      },
+    }
+  )
+);
